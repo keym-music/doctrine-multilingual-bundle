d994c4fc0e7e9651c187239553965b3e6f772112
Julien Gabriel <julien.gabriel@keym-music.com>
2024-01-19 14:15:40 +0100

    :bug: Fixed inconsistency in interface implementation
    Method signature wasn't the same

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,13 +2,12 @@
 
 namespace Keym\MultilingualBundle\DependencyInjection;
 
-
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('doctrine_multilingual');
 
